a5253b96cc978595232fdaac3d0c19a2070f557d
tranngan21 <90614847+tranngan21@users.noreply.github.com>
2022-06-29 02:19:00 +0700

    Update connect.php

--- admincp/config/connect.php
+++ admincp/config/connect.php
@@ -1,5 +1,12 @@
 <?php 
-    $conn = mysqli_connect("localhost" , "root" , "123456" , "bandongho") or die("Kết nối CSDL thất bại!");
-	mysqli_set_charset($conn ,"utf8");
-
+session_start()
+$servername = "mysql8002.site4now.net";
+$username ="a892f6_dongho";
+$password = "Tranngan2022";
+$dbname = "db_a892f6_dongho";
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+if(!$conn){
+	die("COnnection failed".mysqli_connect_error());
+}
+mysqli_query($conn,"set names utf8");
 ?>
