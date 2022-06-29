03b2f7766688f235fdd21692cd47b3b97d4c1f74
tranngan21 <90614847+tranngan21@users.noreply.github.com>
2022-06-29 11:15:58 +0700

    Update connect.php

--- admincp/config/connect.php
+++ admincp/config/connect.php
@@ -1,9 +1,9 @@
 <?php 
 session_start()
-$servername = "mysql8002.site4now.net";
-$username ="a892f6_dongho";
-$password = "Tranngan2022";
-$dbname = "db_a892f6_dongho";
+$servername = "mysql5046.site4now.net";
+$username ="a8933e_dongho";
+$password = "dongho2022";
+$dbname = "db_a8933e_dongho";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 if(!$conn){
 	die("COnnection failed".mysqli_connect_error());
